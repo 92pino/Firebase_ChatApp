10cd615bcae4756d5352ab07133d32a56b7afdef
92pino <jjb8382@gmail.com>
2020-08-18 23:54:01 +0900

    :sparkles:UINavigationBar Appearence:sparkles:

---
+++ Firebase_ChatApp/Firebase_ChatApp/Controller/Authentication/LoginController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginController.swift
+//  Firebase_ChatApp
+//
+//  Created by JinBae Jeong on 2020/08/18.
+//  Copyright © 2020 pino. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+  
+  // Mark: - Properties
+  
+  // Mark: - Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+  
+  // Mark: - Selectors
+
+}
---
+++ Firebase_ChatApp/Firebase_ChatApp/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationController.swift
+//  Firebase_ChatApp
+//
+//  Created by JinBae Jeong on 2020/08/18.
+//  Copyright © 2020 pino. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
