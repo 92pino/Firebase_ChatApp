58e72746779344335443995fe1897599851bb130
92pino <jjb8382@gmail.com>
2020-08-18 23:19:27 +0900

    :tada:Init Chat App:tada:

---
+++ Firebase_ChatApp/Firebase_ChatApp/Controller/ConversationsController.swift
@@ -0,0 +1,24 @@
+//
+//  ConversationsController.swift
+//  Firebase_ChatApp
+//
+//  Created by JinBae Jeong on 2020/08/18.
+//  Copyright © 2020 pino. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+  // Mark: - Properties
+  
+  // Mark: - Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+  
+  // Mark: - Selectors
+
+}
---
+++ Firebase_ChatApp/Firebase_ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase_ChatApp
+//
+//  Created by JinBae Jeong on 2020/08/18.
+//  Copyright © 2020 pino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
